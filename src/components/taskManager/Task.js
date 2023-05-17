c5e852c5ed8c2420b6c45028976bc0ac386423b6
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2023-05-17 13:56:37 +0300

    linking state to form input

--- src/components/taskManager/Task.js
+++ src/components/taskManager/Task.js
@@ -1,28 +1,29 @@
 import React from 'react'
-import {FaCheckDouble, FaEdit,FaTrashAlt} from 'react-icons/fa'
+import { FaCheckDouble, FaEdit, FaTrashAlt } from 'react-icons/fa'
 const Task = () => {
-  return (
-    <div className='task'>
-        <span>
-            <p><strong>Task:</strong>Task one</p>
-            <p><strong>Data:</strong>2023-05-17</p>
-        </span>
-        <span>
-<button>
-    <FaEdit button color='green'/>
-</button>
+    return (
+        <div className='task'>
+            <span>
+                <p><strong>Task:</strong>Task one</p>
+                <p><strong>Data:</strong>2023-05-17</p>
+            </span>
+            <span>
+                
+                    <div className=' flex align-items-center '>
+                        <button  className='btn btn-warning my-1'>
+                            <i className='fa fa-eye' />
+                        </button >
+                        <button  className='btn btn-primary my-1'>
+                            <i className='fa fa-pen' />
+                        </button >
+                        <button  className='btn btn-danger my-1'>
+                            <i className='fa fa-trash' />
+                        </button>
+                    </div>
+            </span>
 
-<button>
-    <FaTrashAlt color='red'/>
-</button>
-<button>
-    <FaCheckDouble color='blue'/>
-</button>
-
-        </span>
-     
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Task
--- src/components/taskManager/TaskManager.js
+++ src/components/taskManager/TaskManager.js
@@ -1,26 +1,40 @@
 import React from 'react'
-
+import { useState } from 'react';
 import Task from "./Task";
 import "./TaskManager.css";
 
 const TaskManager = () => {
+  const[user, setUser]=useState("")
+  const[name, setName]=useState("")
+  const[date, setDate]=useState("")
+  const[tasks, setTasks]=useState([])
+
+  const handleSubmit=(e) => {
+    e.preventDefault();
+       console.log(name);
+       console.log(date);
+  };
+
   return (
     <div className='--bg-primary' >
       <h1 className='--text-center --text-light'>TaskManager</h1>
       <div className='--flex-center --p'>
         <div className='--card --bg-light --width-500px --p --flex-center'>
-            <form  className='form --form-control'>
+            <form  onSubmit={handleSubmit} className='form --form-control'>
             <div>
                 <label htmlFor='name'>Name:</label>
-                <input type='text' placeholder='Task Name' name='name' />
+                <input type='text' placeholder='Task Name' name='name' 
+                value={user} onChange={(e)=>setUser(e.target.value)}/>
                </div>
                <div>
                 <label htmlFor='name'>Task:</label>
-                <input type='text' placeholder='Task Name' name='name' />
+                <input type='text' placeholder='Task Name' name='name'
+                value={date} onChange={(e)=>setDate(e.target.value)} />
                </div>
                <div>
                 <label htmlFor='date'>Data:</label>
-                <input type='date' placeholder='Task Name' name='date' />
+                <input type='date' placeholder='Task Name' name='date' 
+                value={name} onChange={(e)=>setName(e.target.value)}/>
                </div>
                <button className='--btn --btn-success --btn-block'>Task</button>
             </form>
